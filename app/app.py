c47053b2508d49723c29a8b94a881be65e497f62
David Ellinger <david.ellinger@me.com>
2021-01-27 08:55:10 -0500

    Switched over to flask

--- app/app.py
+++ app/app.py
@@ -1,63 +1,76 @@
-from functools import lru_cache
-
+from flask import Flask, jsonify
 from typing import Optional, Iterator
-from fastapi import Depends, FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from fastapi_utils.guid_type import GUID, GUID_DEFAULT_SQLITE
-from fastapi_utils.session import FastAPISessionMaker
-from pydantic import BaseSettings
-import sqlalchemy as sa
 from uuid import UUID
+from redis import Redis
 
-Base = declarative_base()
+redis = Redis(host='redis', port=6379)
 
-class User(Base):
-    __tablename__ = "user"
-    id = sa.Column(GUID, primary_key=True, default=GUID_DEFAULT_SQLITE)
-    name = sa.Column(sa.String, nullable=False)
+def create_app():
+    app = Flask(__name__)
+    # app.config['SQLALCHEMY_DATABASE_URI']
+    return app
 
-class DBSettings(BaseSettings):
-    """ Parses variables from environment on instantiation """
+app = create_app()
 
-    database_uri: str = "sqlite:///todo_app.db" # could break up into scheme, username, password, host, db
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
-
-# def get_db() -> Iterator[Session]:
-#         """ FastAPI dependency that provides a sqlalchemy session """
-#         yield from _get_fastapi_sessionmaker().get_db()
-
-
-# @lru_cache() # reuse session across requests
-# def _get_fastapi_sessionmaker() -> FastAPISessionMaker:
-#     """ This function could be replaced with a global variable if preferred """
-#     database_uri = DBSettings().database_uri
-#     return FastAPISessionMaker(database_uri) # check_same_thread only needed for sqlite
-
-app = FastAPI()
-engine = create_engine(
-    "sqlite:///todo_app.db", connect_args={"check_same_thread": False}
-)
-
-@app.get("/")
+@app.route("/")
 def ping():
-    return {"ping": "pong"}
+    return jsonify(ping="pong")
+
+@app.route('/hit')
+def hello():
+    redis.incr('hits')
+    return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
+
+# Base = declarative_base()
+
+# class User(Base):
+#     __tablename__ = "user"
+#     id = sa.Column(GUID, primary_key=True, default=GUID_DEFAULT_SQLITE)
+#     name = sa.Column(sa.String, nullable=False)
+
+# class DBSettings(BaseSettings):
+#     """ Parses variables from environment on instantiation """
+
+#     database_uri: str = "sqlite:///todo_app.db" # could break up into scheme, username, password, host, db
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# # def get_db() -> Iterator[Session]:
+# #         """ FastAPI dependency that provides a sqlalchemy session """
+# #         yield from _get_fastapi_sessionmaker().get_db()
 
-@app.get("/{user_id}")
-def get_user_name(user_id: int) -> str:
-    db = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
-    test_user = User(name="Darla")
-    db.add(test_user)
-    db.commit()
-    # user = db.query(User).get(user_id)
-    # username = user.name
-    return "Test"
+
+# # @lru_cache() # reuse session across requests
+# # def _get_fastapi_sessionmaker() -> FastAPISessionMaker:
+# #     """ This function could be replaced with a global variable if preferred """
+# #     database_uri = DBSettings().database_uri
+# #     return FastAPISessionMaker(database_uri) # check_same_thread only needed for sqlite
+
+# app = FastAPI()
+# engine = create_engine(
+#     "sqlite:///todo_app.db", connect_args={"check_same_thread": False}
+# )
+
+# @app.get("/")
+# def ping():
+#     return {"ping": "pong"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
+
+# @app.get("/{user_id}")
+# def get_user_name(user_id: int) -> str:
+#     db = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
+#     test_user = User(name="Darla")
+#     db.add(test_user)
+#     db.commit()
+#     # user = db.query(User).get(user_id)
+#     # username = user.name
+#     return "Test"
 
 
 # def create_app():
@@ -81,6 +94,4 @@ def get_user_name(user_id: int) -> str:
 #     app.config.from_object(f"app.config.{config_name}.Config")
 
 
-# # This is required for flask to bootstrap without gunicorn for development
-# if os.environ.get("FLASK_DEBUG"):
-#     app = create_app()
+
